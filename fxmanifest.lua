f898603c341db36dd1f1af95a9795ec71fcc83ec
Preacher <68681418+PreacherDev@users.noreply.github.com>
2022-03-07 20:43:32 +0100

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author '[LIFESERVICES/LIFE SCRIPTS]//code by Alpha#7721 and Preacher#6392'
+description 'Elevator Script for ESX'
+version '1.0'
+
+client_scripts {
+  '@NativeUI/NativeUI.lua',
+  'config.lua',
+  'client/client.lua',
+}
